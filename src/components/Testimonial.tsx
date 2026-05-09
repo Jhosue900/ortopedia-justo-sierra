84f4d9d00ef1f88d5ad1dcf99af7f86a236b6d86
jhon-henao13 <jhonestiwarhenao@gmail.com>
2026-05-09 16:01:59 -0500

    fix(testimonial): videos comentados por ahora

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -102,8 +102,7 @@ export default function Testimonial() {
         </div>
 
         {/* Grid de Videos */}
-                {/* Grid de Videos */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 mt-12">
+        {/*<div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 mt-12">
           {videoTestimonials.map((video, i) => (
             <motion.div
               key={i}
@@ -117,11 +116,11 @@ export default function Testimonial() {
                 preload="metadata"
                 playsInline
               />
-              {/* Borde decorativo */}
+              
               <div className="absolute inset-0 border-[8px] border-white/10 rounded-3xl pointer-events-none" />
             </motion.div>
           ))}
-        </div>
+        </div>*/}
 
 
       </div>
